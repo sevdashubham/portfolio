28f96f8a4ccb1a31404dc570c58a37c03f61a5fd
Shubham Sevda <sevdashubham@gmail.com>
2024-09-21 00:55:26 +0530

    push about section

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,13 @@
+export default function Spinner() {
+    return <div className="lds-grid">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+}
