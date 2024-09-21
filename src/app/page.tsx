17879ddb434c02a124f02d4fa9b9987bf25b1754
Shubham Sevda <sevdashubham@gmail.com>
2024-09-22 02:18:46 +0530

    final fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
-import Spline from '@splinetool/react-spline/next';
 import Link from "next/link";
+import dynamic from "next/dynamic";
 import {Suspense} from "react";
 import Spinner from "@/components/Spinner";
 import About from "@/modules/about/about";
 import Header from "@/components/Header";
 
+const DynamicSpline = dynamic(() => import('@/components/Spline'), {
+    ssr: false,
+    loading: () => <Spinner/>,
+})
 export default function Home() {
     return (
         <main className="w-full h-full bg-white dark:bg-black">
-            <Header />
+            <Header/>
             <div className="px-4">
-            <div className="h-[90vh] w-full flex items-center justify-center rounded-b-3xl overflow-hidden">
-                <Suspense fallback={<Spinner/>}>
-                    <Spline
-                        className="h-[80vh]"
-                        scene="https://prod.spline.design/WrQluddYjv3nMHVG/scene.splinecode"
-                    />
-                </Suspense>
-            </div>
+                <div className="h-[90vh] w-full flex items-center justify-center rounded-b-3xl overflow-hidden">
+                    <Suspense fallback={<Spinner/>}>
+                        <DynamicSpline/>
+                    </Suspense>
+                </div>
             </div>
             <Link href={'#work'}>
                 <div id='more' className="pt-6 pb-6 flex items-center justify-center cursor-pointer">
-                    <div className="scroll" />
+                    <div className="scroll"/>
                 </div>
             </Link>
             <About/>
---
+++ src/components/Spline.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline/next';
+
+
+export default function SplineComponent() {
+    return <Spline
+        className="h-[80vh]"
+        scene="https://prod.spline.design/WrQluddYjv3nMHVG/scene.splinecode"
+    />
+}
